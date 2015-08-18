7b0762189b4cfe8846755b0dd79f1ca3d1517526
Anand Krishnan <anand230999@gmail.com>
2015-08-18 16:07:01 +0530

    Create 2-01.c

---
+++ 2-01.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+# include<float.h>
+# include<limits.h>
+main()
+{
+	printf("CHAR MIN %d\n",CHAR_MIN);
+	printf("CHAR MAX %d\n",CHAR_MAX);
+	printf("SIGNED CHAR MIN %d\n",SCHAR_MIN);
+	printf("SIGNED CHAR MAX %d\n",SCHAR_MAX);
+	printf("UNSIGNED CHAR MAX %d\n",UCHAR_MAX);
+	printf("INT MIN %d\n",INT_MIN);
+	printf("INT MAX %d\n",INT_MAX);
+	printf("UNSIGNED INT MAX %d\n",UINT_MAX);
+	printf("LONG MIN %ld\n",LONG_MIN);
+	printf("LONG MAX %ld\n",LONG_MAX);
+	printf("UNSIGNED LONG MAX %lu\n",ULONG_MAX);
+	printf("SHORT MIN %d\n",SHRT_MIN);
+	printf("SHORT MAX %d\n",SHRT_MAX);
+	printf("UNSIGNED SHORT MAX %d\n",USHRT_MAX);
+
+	
+}
